0866a0518409e6b113cf45a3da14174284fc01e6
mogog97 <mario.mog.111@gmail.com>
2022-02-14 13:02:42 +0100

    Componente1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Componente1Component } from './componente1/componente1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component
   ],
   imports: [
     BrowserModule
